07f53228241c80830465a89a0a00293f2741d993
Camilla Callierotti <camillacallierotti@Camillas-Air.homenet.telecomitalia.it>
2021-06-18 16:35:58 +0200

    fixed cox and rsf predictions

--- cph_rsf_comparison.R
+++ cph_rsf_comparison.R
@@ -1,7 +1,11 @@
 ### packages
 
+library(tidyverse)
+library(dplyr)
+library(pec)
 library(rms)
 library(glmnet)
+library(vimp)
 library(randomForestSRC)
 library(ggRandomForests)
 library(boot)
@@ -10,35 +14,86 @@ library(boot)
 
 data(pbc, package = "randomForestSRC")
 pbc <- pbc[complete.cases(pbc),]
+set.seed(1998)
+index <- sample(1:276, size = trunc(.8 * 276)) # 80:20 train-test split
+pbc_train <- pbc %>%
+  filter(row_number() %in% index)
+pbc_test <- pbc %>%
+  filter(!(row_number() %in% index))
 
-### single cox regression
+### compare full cox and RSF predictions
 
-full.cox = coxph(Surv(days, status) ~ ., data = pbc)
-summary(full.cox)
-write.csv(round(coef(full.cox), 2))
+# fit and predict cox
 
-### single rsf
+cox.fit = coxph(Surv(days, status) ~ ., data = pbc_train, x = TRUE)
+summary(cox.fit)
+write.table(format(coef(cox.fit), scientific=TRUE))
+cox.pred <- predictSurvProb(cox.fit, newdata = pbc_test, times = 10 * 365.25) # predicted 10 year survival probabilities for individuals in test set (n=56)
 
-rsf <- rfsrc(Surv(days, status) ~ ., pbc)
-print(rsf)
+# fit and predict RSF
 
-### compare cox lasso variable selection and rsf vimp
+rsf.fit <- rfsrc(Surv(days, status) ~ ., pbc_train)
+print(rsf.fit)
+rsf.pred <- predictSurvProb(rsf.fit, newdata = pbc_test, times = 10 * 365.25) # predicted 10 year survival probabilities for individuals in test set (n=56)
+
+# select 3 individuals to make predictions for
+
+which.min(pbc_test$age) # row 20 has individual with min age
+which.max(pbc_test$age) # row 43 has individual with max age
+quantile(pbc_test$age, .5, type = 1) # 16941 days is median age
+which(pbc_test$age == 16941) # row 46 has individual with median age
+
+# predictions for 3 individuals
+
+lapply(c(20,46,43), function(x) {
+  (predictSurvProb(cox.fit, newdata = pbc_test[x, ], times = 10 * 365.25))*100
+}) # predicted 10 year survival probabilities with Cox for 3 test individuals of different ages
+
+lapply(c(20,46,43), function(x) {
+  (predictSurvProb(rsf.fit, newdata = pbc_test[x, ], times = 10 * 365.25) )*100
+}) # predicted 10 year survival probabilities with RSF for 3 test individuals of different ages
+
+# predicted survival curves for 3 individuals
+
+par(mfrow = c(1, 3))
+
+lapply(c(20,46,43), function(x) {
+  plotPredictSurvProb(cox.fit, newdata = pbc_test[x, ], lty = 1) 
+  plotPredictSurvProb(rsf.fit, newdata = pbc_test[x, ], add = TRUE, lty = 2) 
+}) # predicted survival curves with Cox for 3 test individuals of different ages
+
+### compare cox and rsf vimp
 
 # penalised cox variable selection 
 
 x <- as.matrix(pbc[3:ncol(pbc)])
 y <- Surv(pbc$days, pbc$status)
 
-cox.fit <- glmnet(x, y, family="cox", alpha=1)
-plot(cox.fit, label=T)
+lasso.fit <- glmnet(x, y, family="cox", alpha=1)
+plot(lasso.fit, label=T)
 
-cox.cv <- cv.glmnet(x, y, family="cox", alpha=1)
-cox.cv$lambda.1se # lambda = 0.1343436
-plot(cox.cv)
+lasso.cv <- cv.glmnet(x, y, family="cox", alpha=1)
+lasso.cv$lambda.1se # lambda = 0.1343436
+plot(lasso.cv)
 
-coef(cv.fit, s = "lambda.1se")
+coef(lasso.fit, s = "lambda.1se")
 
-# rsf vimp
+# cox vimp
+
+cox.vimp <- anova(full.cox)
+plot(cox.vimp,
+     what="proportion R2", #proportion of overall model R2 that is due to each predictor
+     xlab=NULL, pch=16,
+     rm.totals=TRUE, rm.ia=FALSE, rm.other=NULL,
+     sort="descending", margin='P',
+     pl=TRUE, trans=NULL, ntrans=40, height=NULL, width=NULL)
+
+# alternative cox vimp
+
+x <- as.data.frame(x)
+vimp_rsquared(y, x)
+
+# rsf vimp 
 
 rsf.fit <- rfsrc(Surv(days, status) ~ ., data = pbc)
 

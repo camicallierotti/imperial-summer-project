0402f1b34817b382c12b99be66668bc725c45133
Camilla Callierotti <camillacallierotti@Camillas-Air-2.homenet.telecomitalia.it>
2021-08-20 03:22:55 +0200

    train test

--- boosting_pbc.R
+++ boosting_pbc.R
@@ -14,13 +14,21 @@ pbc_train <- pbc %>%
 pbc_test <- pbc %>%
   filter(!(row_number() %in% index))
 
+### datasets
+
+# create survival label (y) for training
+y_train <- ifelse(pbc_train$status == 1, pbc_train$days, -pbc_train$days) # equivalent to surv object
+
+# create predictors (X) for training
+X_train <- pbc_train %>% select(-days,-status)
+
+# create survival label (y) for testing
+y_test <- ifelse(pbc_test$status == 1, pbc_test$days, -pbc_test$days) # equivalent to surv object
+
+# create predictors (X) for testing
+X_test <- pbc_test %>% select(-days,-status)
+
 ### xgboost
-
-# create survival label (y)
-y <- ifelse(pbc$status == 1, pbc$days, -pbc$days) # equivalent to surv object
-
-# create predictors (X)
-X <- pbc %>% select(-days,-status)
 
 # set parameters
 params <- list(objective = "survival:cox", # what type of dependent variable we have
@@ -28,8 +36,8 @@ params <- list(objective = "survival:cox", # what type of dependent variable we 
 )
 
 # run xgboost
-model <- xgboost(data = as.matrix(X), # xgboost library requires data as matrix
-                 label = y,
+model <- xgboost(data = as.matrix(X_train), # xgboost library requires data as matrix
+                 label = y_train,
                  params = params,
                  nrounds = 20, # number of times we want xgboost to run
                  verbose = 1
@@ -38,12 +46,24 @@ model <- xgboost(data = as.matrix(X), # xgboost library requires data as matrix
 # shap values (characteristics that correlate the most)
 xgb.plot.shap(data = as.matrix(X),
               model = model,
-              top_n = 5)
+              top_n = 4,
+              cex.lab=1.5,
+              cex.axis=1.5)
 
 # make predictions
+pred <- predict(model, as.matrix(X_test))
 
-pred <- predict(model, as.matrix(X))
+# c-index of prediction
+cindex <- survConcordance(y_test ~ pred) # 0.33 bc pred is surv probabilities but y_test is time+status
 
-# 10-year survival probabilities
+### 10-year survival probabilities
 
-cindex <- survConcordance(y ~ pred)
+# find row numbers with the 3 individuals to make predictions for
+which.min(X_test$age) # row 20 has individual with min age
+which.max(X_test$age) # row 43 has individual with max age
+quantile(X_test$age, .5, type = 1) # 16941 days is median age
+which(X_test$age == 16941) # row 46 has individual with median age
+
+pred[20] # min
+pred[43] # max
+pred[46] # median

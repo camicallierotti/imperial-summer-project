828454a93b2d5c47f73930f468048220be711347
Camilla Callierotti <camillacallierotti@Camillas-Air.homenet.telecomitalia.it>
2021-08-22 12:16:51 +0200

    made predictions with xgboost on pbc

--- boosting_pbc.R
+++ boosting_pbc.R
@@ -28,7 +28,7 @@ y_test <- ifelse(pbc_test$status == 1, pbc_test$days, -pbc_test$days) # equivale
 # create predictors (X) for testing
 X_test <- pbc_test %>% select(-days,-status)
 
-### xgboost
+### train xgboost
 
 # set parameters
 params <- list(objective = "survival:cox", # what type of dependent variable we have
@@ -36,34 +36,33 @@ params <- list(objective = "survival:cox", # what type of dependent variable we 
 )
 
 # run xgboost
-model <- xgboost(data = as.matrix(X_train), # xgboost library requires data as matrix
+xgboost_model <- xgboost(data = as.matrix(X_train), # xgboost library requires data as matrix
                  label = y_train,
                  params = params,
-                 nrounds = 20, # number of times we want xgboost to run
+                 max_depth = 2, # don't need deep trees bc small dataset
+                 nrounds = 2, # number of times we want xgboost to run
+                 eta = 1,
                  verbose = 1
-)
+                 )
 
-# shap values (characteristics that correlate the most)
-xgb.plot.shap(data = as.matrix(X),
+### make predictions
+
+pred <- predict(xgboost_model, as.matrix(X_test)) # perform the prediction of risk scores
+print(length(pred)) # size of the prediction vector (confirm 56 same as test set)
+print(head(pred)) # display first predictions: probabilities that a datum will be classified as 1 (event happening)
+prediction <- as.numeric(pred > 0.5) # convert to probability of dead or alive
+
+### c-index 
+
+y_for_cindex <- ifelse(y_test < 0, 0, 1) # convert censored to 0 and death to 1
+cindex <- survConcordance(y_for_cindex ~ prediction) # 0.17???
+
+### variable importance
+
+# shap values
+xgb.plot.shap(data = as.matrix(X_train),
               model = model,
               top_n = 4,
               cex.lab=1.5,
               cex.axis=1.5)
 
-# make predictions
-pred <- predict(model, as.matrix(X_test))
-
-# c-index of prediction
-cindex <- survConcordance(y_test ~ pred) # 0.33 bc pred is surv probabilities but y_test is time+status
-
-### 10-year survival probabilities
-
-# find row numbers with the 3 individuals to make predictions for
-which.min(X_test$age) # row 20 has individual with min age
-which.max(X_test$age) # row 43 has individual with max age
-quantile(X_test$age, .5, type = 1) # 16941 days is median age
-which(X_test$age == 16941) # row 46 has individual with median age
-
-pred[20] # min
-pred[43] # max
-pred[46] # median

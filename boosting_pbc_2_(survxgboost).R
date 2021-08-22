32ec90b82b2142f30e8bb3e9757698038fb1912a
Camilla Callierotti <camillacallierotti@Camillas-Air.homenet.telecomitalia.it>
2021-08-22 12:17:14 +0200

    plotted full predicted survival on pbc with survXgboost

--- boosting_pbc_2_(survxgboost).R
+++ boosting_pbc_2_(survxgboost).R
@@ -27,7 +27,7 @@ surv_xgboost_model <- xgb.train.surv(
   nrounds = 1000, early_stopping_rounds = 30
 )
 
-### predict survival curves
+### predict 
 
 # find individuals to predict for
 x_train_df <- as.data.frame(x_train)
@@ -35,14 +35,22 @@ which(x_train_df$age == 10550) # row 76
 which(x_train_df$age == 16941) # row 206
 which(x_train_df$age == 25006) # row 195
 
-# select survival curves
-survival_curves_selected <- survival_curves[c(76, 206, 195),]
-
-# predict
-times <- seq(10, 1000, 50)
+# predict full survival curves
+times <- seq(10, 4000, 50)
 survival_curves <- predict(object = surv_xgboost_model, newdata = x_train, type = "surv", times = times)
-matplot(times, t(survival_curves_selected), type = "l")
+survival_curves_selected <- survival_curves[c(76, 206, 195),] # select curves for 3 individuals
+matplot(times, t(survival_curves_selected),
+        type = "l", lty = "solid", lwd = 2, cex = 2.5,
+        col = c("orange", "blue", "red"),
+        xlab = "Time", ylab = "Survival probability")
+legend(x = "bottomleft",inset = 0,
+       legend = c("Patient 1", "Patient 2", "Patient 3"),
+       col = c("orange", "blue", "red"),
+       lty = "solid", lwd = 2, cex = 1)
 
 # predict risk score
-risk_scores <- predict(object = surv_xgboost_model, newdata = x_train, type = "risk")
+risk_scores <- predict(object = surv_xgboost_model, newdata = x_train[c(76, 206, 195),], type = "risk")
 hist(risk_scores)
+
+# predict 10-year survival prob
+

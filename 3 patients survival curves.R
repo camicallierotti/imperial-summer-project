4db531e41aed6e8122d524c5ff14ef7d6343a1ec
Camilla Callierotti <camillacallierotti@Camillas-Air.homenet.telecomitalia.it>
2021-08-23 14:28:49 +0200

    completed survival curves (cph, rsf, xgb)

--- 3 patients survival curves.R
+++ 3 patients survival curves.R
@@ -1,33 +1,69 @@
 ### three individuals survival curves
 
-# make df with 3 individuals
+## pbc data
 
-data(pbc, package = "randomForestSRC")
-pbc <- pbc[complete.cases(pbc),]
-patient1 <- pbc[270,]
-patient2 <- pbc[104,]
-patient3 <- pbc[253,]
+data('pbc', package='randomForestSRC')
+pbc <- pbc %>% filter(complete.cases(.))
+pbc <- pbc %>% 
+  mutate(stage = as.factor(stage),
+         edema = as.factor(edema),
+         age = age/365.25,
+         treatment = as.factor(treatment)) 
+set.seed(1998)
+index <- sample(1:276, size = trunc(.8 * 276)) # 80:20 train-test split
+pbc_train <- pbc %>%
+  filter(row_number() %in% index)
+pbc_test <- pbc %>%
+  filter(!(row_number() %in% index))
+X_train <- model.matrix(Surv(days, status)~., data=pbc_train)
+y_train <- Surv(pbc_train$days, pbc_train$status)
+X_test <- model.matrix(Surv(days, status)~., data=pbc_test)
+y_test <- Surv(pbc_test$days, pbc_test$status)
 
-#quantile(pbc$age, .5, type = 1) 
-#which(pbc$age == 18137) # row 104
+## xgboost plot data
 
-pbc_patients <- pbc[c(270,104,253),]
+xgb_plt_dat <- readRDS("~/OneDrive - Imperial College London/AstraZeneca/imperial-summer-project/xgb_survpred_plotdata.RDS")
 
-# cox
+## cox ph plot data
 
-cox.fit = coxph(Surv(days, status) ~ ., data = pbc, x = TRUE, y = TRUE)
+cox.fit = coxph(Surv(days, status) ~ ., data = pbc_train, x = TRUE, y = TRUE)
+cox.pred <- predictSurvProb(cox.fit, newdata = pbc_test[c(20,46,43),], times = 365.25*seq(0,10,by=0.5), type='surv') 
+cox.pred <- as.data.frame(t(cox.pred))
+cox.pred <- cbind(xgb_plt_dat$times, cox.pred)
+colnames(cox.pred)[1] <-"times"
 
-# rsf
+## rsf plot data
 
-rsf.fit <- rfsrc(Surv(days, status) ~ ., data = pbc)
+rsf.fit <- rfsrc(Surv(days, status) ~ ., pbc_train)
+rsf.pred <- predictSurvProb(rsf.fit, newdata = pbc_test[c(20,46,43),], times = 365.25*seq(0,10,by=0.5), type='surv') 
+rsf.pred <- as.data.frame(t(rsf.pred))
+rsf.pred <- cbind(xgb_plt_dat$times, rsf.pred)
+colnames(rsf.pred)[1] <-"times"
 
-# xgb
+## plots
+
+plot1 <- ggplot(NULL, aes(times, V1)) + 
+  geom_step(data = cox.pred, color='red') +
+  geom_step(data = rsf.pred, color='blue') +
+  geom_step(data = xgb_plt_dat, color='green') +
+  labs(x="Time (years)", y="Survival probability", title="Probability of survival of minimum age") +
+  theme_bw()
+
+plot2 <- ggplot(NULL, aes(times, V2)) + 
+  geom_step(data = cox.pred, color='red') +
+  geom_step(data = rsf.pred, color='blue') +
+  geom_step(data = xgb_plt_dat, color='green') +
+  labs(x="Time (years)", y="Survival probability", title="Probability of survival of median age") +
+  theme_bw()
+
+plot3 <- ggplot(NULL, aes(times, V3)) + 
+  geom_step(data = cox.pred, color='red') +
+  geom_step(data = rsf.pred, color='blue') +
+  geom_step(data = xgb_plt_dat, color='green') +
+  labs(x="Time (years)", y="Survival probability", title="Probability of survival of maximum age") +
+  theme_bw()
+
+require(gridExtra)
+grid.arrange(plot1, plot2, plot3, ncol=3)
 
 
-
-# plot for patient 1
-
-plotPredictSurvProb(cox.fit, newdata = patient1, lty = 1, legend = TRUE, percent = TRUE) 
-plotPredictSurvProb(rsf.fit, newdata = patient1, lty = 1, legend = TRUE, percent = TRUE) 
-
-predict(object = surv_xgboost_model, newdata = patient1, type = "surv", times = times)
